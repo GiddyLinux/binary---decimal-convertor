7257f2af009c63aa3e39ccfa88f468f25aeeaf7f
giddy <giddy@duck.com>
2023-01-27 13:40:01 +0000

    Remove debug directory

--- target/debug/binary-decimal.d
+++
@@ -1,1 +0,0 @@
-/home/toney/stuff/IT-stuff/coding/rust/binary-decimal/target/debug/binary-decimal: /home/toney/stuff/IT-stuff/coding/rust/binary-decimal/src/main.rs
--- target/debug/deps/binary_decimal-883109d4a6d8a53d.d
+++
@@ -1,7 +0,0 @@
-/home/toney/stuff/IT-stuff/coding/rust/binary-decimal/target/debug/deps/binary_decimal-883109d4a6d8a53d.rmeta: src/main.rs
-
-/home/toney/stuff/IT-stuff/coding/rust/binary-decimal/target/debug/deps/binary_decimal-883109d4a6d8a53d.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
